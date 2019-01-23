de950b4d931e2eb8cf33d2897eec5c65d888f226
RomainJunca <romain.junca@viacesi.fr>
2019-01-23 14:44:10 +0100

    Wall behavior towards camera

---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+    public GameObject cam;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        gameObject.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y-1.5f, cam.transform.position.z + 1);
+
+	}
+}
