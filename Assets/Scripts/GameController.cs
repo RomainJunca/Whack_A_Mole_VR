91ed1efb6fc4fbdd2317fc312a0c02bd15a90d8c
TngBlt <tanguy21b@hotmail.fr>
2019-02-04 09:10:58 +0100

    fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,9 +14,14 @@ public class GameController : MonoBehaviour {
 		
 	}
 
+	// This function will be called everytime the user is pressing the trigger
+	// If it hits a mole, then isAMole is true.
 	void MoleWhackDetection(bool isAMole) {
 		if(isAMole) {
 			// Do what you want when the user is whacking a mole
+		}
+		else {
+			//
 		}
 	}
 }
