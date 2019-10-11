09a2f080f81ac1724353218da13f4efc42bf68fa
QuentinDaveau <daveau.quentin@hotmail.fr>
2019-10-11 11:23:57 +0200

    Add working abstract Pointer class and BasicPointer test class
    The Pointer class is the abstract class used by the controller to point
    and pop Mole objects. The
    BasicPointer is an implementation of the Pointer class.

---
+++ Assets/Scripts/BasicPointer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Basic implementation of the pointer abstract class. Simply changes the color of the laser and 
+the "cursor" on shoot.
+*/
+
+public class BasicPointer : Pointer
+{
+    [SerializeField]
+    private Color shootColor;
+
+    private delegate void Del();
+    protected override void PlayShoot()
+    {
+        laser.startColor = shootColor;
+        laser.endColor = shootColor;
+        cursorRenderer.material.color = shootColor;
+        StartCoroutine(Wait(.2f, new Del(ResetLaser)));
+    }
+
+    private void ResetLaser()
+    {
+        laser.startColor = startLaserColor;
+        laser.endColor = EndLaserColor;
+        cursorRenderer.material.color = EndLaserColor;
+        base.PlayShoot();
+    }
+
+    private IEnumerator Wait(float duration, Del method)
+    {
+        yield return new WaitForSeconds(duration);
+        method();
+    }
+}
---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+/*
+Abstract class of the VR pointer used to pop moles. Like the Mole class, calls specific empty
+functions on events to be overriden in its derived classes.
+*/
+
+public abstract class Pointer : MonoBehaviour
+{
+    private enum States {Idle, Shooting}
+
+    [SerializeField]
+    private SteamVR_Input_Sources controller;
+
+    [SerializeField]
+    protected Vector3 laserOrigin;
+
+    [SerializeField]
+    protected Color startLaserColor;
+
+    [SerializeField]
+    protected Color EndLaserColor;
+
+    [SerializeField]
+    protected float laserWidth = .1f;
+
+    [SerializeField]
+    protected Material laserMaterial;
+
+    [SerializeField]
+    protected float maxLaserLength;
+
+    [SerializeField]
+    protected GameObject cursor;
+
+    protected LineRenderer laser;
+    protected Renderer cursorRenderer;
+    private States state = States.Idle;
+
+    private Mole hoveredMole;
+    
+    void Start()
+    {
+        InitLaser();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + laserOrigin, transform.forward, out hit, 100f))
+        {
+            UpdateLaser(true, hit.distance);
+            hoverMole(hit);
+        }
+        else
+        {
+            UpdateLaser(false, maxLaserLength);
+        }
+
+        if(SteamVR.active)
+        {
+            if (SteamVR_Input._default.inActions.GrabPinch.GetStateDown(controller))
+            {
+                if (state == States.Idle)
+                {
+                    Shoot(hit);
+                }
+            }
+        }
+        
+        //test input
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (state == States.Idle)
+            {
+                Shoot(hit);
+            }
+        }
+    }
+
+    protected virtual void PlayShoot() 
+    {
+        state = States.Idle;
+    }
+
+    private void hoverMole(RaycastHit hit)
+    {
+        Mole mole;
+        if (hit.collider.gameObject.TryGetComponent<Mole>(out mole))
+        {
+            if (mole != hoveredMole)
+            {
+                if (hoveredMole)
+                {
+                    hoveredMole.OnHoverLeave();
+                }
+                hoveredMole = mole;
+                hoveredMole.OnHoverEnter();
+            }
+        }
+        else
+        {
+            if (hoveredMole)
+            {
+                hoveredMole.OnHoverLeave();
+                hoveredMole = null;
+            }
+        }
+    }
+
+    private void Shoot(RaycastHit hit)
+    {
+        Mole mole;
+        state = States.Shooting;
+        if (hit.collider.gameObject.TryGetComponent<Mole>(out mole))
+        {
+            mole.Pop();
+        }
+        PlayShoot();
+    }
+
+    private void UpdateLaser(bool hit, float distance)
+    {
+        laser.SetPosition(1, laserOrigin + Vector3.forward * distance);
+        if (cursorRenderer.enabled != hit)
+        {
+            cursorRenderer.enabled = hit;
+        }
+        if (hit)
+        {
+            cursor.transform.localPosition = laserOrigin + Vector3.forward * distance;
+        }
+    }
+
+    private void InitLaser()
+    {
+        laser = gameObject.AddComponent<LineRenderer>();
+        laser.useWorldSpace = false;
+        laser.material = laserMaterial;
+        laser.SetPositions(new Vector3[2]{laserOrigin, laserOrigin + Vector3.forward * maxLaserLength});
+        laser.startColor = startLaserColor;
+        laser.endColor = EndLaserColor;
+        laser.startWidth = laserWidth;
+        laser.endWidth = laserWidth;
+
+        if (cursor)
+        {
+            cursorRenderer = cursor.GetComponent<Renderer>();
+            cursorRenderer.material.color = EndLaserColor;
+            cursorRenderer.enabled = false;
+        }
+    }
+
+}
