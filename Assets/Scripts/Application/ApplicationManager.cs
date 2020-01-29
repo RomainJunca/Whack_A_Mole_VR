489319fe069bd5e4aaa39a097a3e90cb8bc23bd5
QuentinDaveau <daveau.quentin@hotmail.fr>
2020-01-29 11:40:28 +0100

    Add exit game key
    Added the possibility to exit the game by pressing the "escape" key.

---
+++ Assets/Scripts/Application/ApplicationManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape")) Application.Quit();
+    }
+}
